eb2361814cb793ce67f25eaadf26d809dd5fdba7
armmer1@gmail.com <armmer1@gmail.com@ba272376-deb0-6960-ef88-aa589f6391d2>
2012-02-02 19:38:07 +0000

    Add map model for sharing process ^^

--- trunk/business-process-query/platform/src/com/signavio/warehouse/query/business/ActivityType.java
+++ trunk/business-process-query/platform/src/com/signavio/warehouse/query/business/ActivityType.java
@@ -2,4 +2,14 @@ package com.signavio.warehouse.query.business;
 
 public enum ActivityType {
 	startEvent, sequenceFlow, endEvent, exclusiveGateway, parallelGateway, task, subProcess, inclusiveGateway;
+	
+	public static boolean contains(String type) {
+
+	    for (ActivityType c : ActivityType.values()) {
+	        if (c.name().equals(type)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
 }

5cf5217a804fcc6b4bd6a826cb76ace6ec480245
armmer1@gmail.com <armmer1@gmail.com@ba272376-deb0-6960-ef88-aa589f6391d2>
2012-02-04 16:24:03 +0000

    add compute and persist service neighbors during sharing process

---
+++ trunk/business-process-query/platform/src/com/signavio/warehouse/query/util/IConstant.java
@@ -0,0 +1,13 @@
+package com.signavio.warehouse.query.util;
+
+public class IConstant {
+	public static final double PIVOT = 0.5;
+	public static final double SCALE_VALUE = 2.0;
+
+	public static final int LEVENSTEIN = 1;
+	public static final int LEVENSTEIN_WITH_ZONEWEIGHT = 2;
+	public static final int IMPROVED_WEIGHT = 3;
+	public static final int IMPROVED_WEIGHT_WITH_ZONEWEIGHT = 4;
+	
+	public static final int NO_OF_MAX_ZONE = 5;
+}

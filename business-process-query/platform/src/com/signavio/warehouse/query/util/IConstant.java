28416eaba98ac39dae5ec7dba56a3e4e2c08095d
armmer1 <armmer1@gmail.com>
2012-02-12 02:41:06 +0000

    Add displaying query results using grid component!

--- business-process-query/platform/src/com/signavio/warehouse/query/util/IConstant.java
+++ business-process-query/platform/src/com/signavio/warehouse/query/util/IConstant.java
@@ -10,4 +10,14 @@ public class IConstant {
 	public static final int IMPROVED_WEIGHT_WITH_ZONEWEIGHT = 4;
 	
 	public static final int NO_OF_MAX_ZONE = 5;
+	
+	public static String getMethodName(int i){
+		String name = "";
+		if(i == IConstant.LEVENSTEIN || i == IConstant.LEVENSTEIN_WITH_ZONEWEIGHT ){
+			name = "Levenstein";
+		}else if(i == IConstant.IMPROVED_WEIGHT || i == IConstant.IMPROVED_WEIGHT_WITH_ZONEWEIGHT){
+			name = "Improved weight";
+		}
+		return name;
+	}
 }

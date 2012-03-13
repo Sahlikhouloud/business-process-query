ae36fe7e9e0679573d1058425a1db0f4daad8229
armmer1@gmail.com <armmer1@gmail.com@ba272376-deb0-6960-ef88-aa589f6391d2>
2012-03-13 00:58:38 +0000

    can compute matching value for new patterns - intermediateTrhow, intermediateCatch, complexGateway, eventGateway

---
+++ trunk/business-process-query/platform/src/com/signavio/warehouse/query/util/LevenshteinForPatterns.java
@@ -0,0 +1,90 @@
+package com.signavio.warehouse.query.util;
+
+public class LevenshteinForPatterns {
+	public static int getLevenshteinDistance(String pattern1, String pattern2) {
+		if (pattern1 == null || pattern2 == null) {
+			throw new IllegalArgumentException("Strings must not be null");
+		}
+
+		/*
+		 * The difference between this impl. and the previous is that, rather
+		 * than creating and retaining a matrix of size s.length()+1 by
+		 * t.length()+1, we maintain two single-dimensional arrays of length
+		 * s.length()+1. The first, d, is the 'current working' distance array
+		 * that maintains the newest distance cost counts as we iterate through
+		 * the characters of String s. Each time we increment the index of
+		 * String t we are comparing, d is copied to p, the second int[]. Doing
+		 * so allows us to retain the previous cost counts as required by the
+		 * algorithm (taking the minimum of the cost count to the left, up one,
+		 * and diagonally up and to the left of the current cost count being
+		 * calculated). (Note that the arrays aren't really copied anymore, just
+		 * switched...this is clearly much better than cloning an array or doing
+		 * a System.arraycopy() each time through the outer loop.)
+		 * 
+		 * Effectively, the difference between the two implementations is this
+		 * one does not cause an out of memory condition when calculating the LD
+		 * over two very large strings.
+		 */
+		String[] s = pattern1.split("\\|\\|");
+		String[] t = pattern2.split("\\|\\|");
+		int n = s.length; // length of s
+		int m = t.length; // length of t
+
+		if (n == 0) {
+			return m;
+		} else if (m == 0) {
+			return n;
+		}
+
+		int p[] = new int[n + 1]; // 'previous' cost array, horizontally
+		int d[] = new int[n + 1]; // cost array, horizontally
+		int _d[]; // placeholder to assist in swapping p and d
+
+		// indexes into strings s and t
+		int i; // iterates through s
+		int j; // iterates through t
+
+		String t_j; // jth character of t
+
+		int cost; // cost
+
+		for (i = 0; i <= n; i++) {
+			p[i] = i;
+		}
+
+		for (j = 1; j <= m; j++) {
+			t_j = t[j - 1];
+			d[0] = j;
+
+			for (i = 1; i <= n; i++) {
+				cost = s[i - 1].equals(t_j) ? 0 : 1;
+				// minimum of cell to the left+1, to the top+1, diagonally left
+				// and up +cost
+				d[i] = Math.min(Math.min(d[i - 1] + 1, p[i] + 1), p[i - 1]
+						+ cost);
+			}
+
+			// copy current distance counts to 'previous row' distance counts
+			_d = p;
+			p = d;
+			d = _d;
+		}
+
+		// our last action in the above loop was to switch d and p, so p now
+		// actually has the most recent cost counts
+		return p[n];
+	}
+
+	public static double directLinkPatternMatching(String pattern1,
+			String pattern2) {
+		double result = 0.0;
+		double distance = getLevenshteinDistance(pattern1, pattern2);
+		String[] s = pattern1.split("\\|\\|");
+		String[] t = pattern2.split("\\|\\|");
+		double noOfString = s.length > t.length ? s.length : t.length;
+
+		result = 1 - (distance / noOfString);
+		return result;
+	}
+
+}
